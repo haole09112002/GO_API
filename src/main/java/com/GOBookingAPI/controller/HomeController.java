c7f09e22a130df10c1a0daae6298e36053cb668a
Tobi <dinhnguyen2002asd@gmail.com>
2023-11-10 16:10:28 +0700

    authentication google

--- src/main/java/com/GOBookingAPI/controller/HomeController.java
+++ src/main/java/com/GOBookingAPI/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.GOBookingAPI.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,10 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 public class HomeController {
 	 @GetMapping("/index")
 	    public String hello(Model model) {
-<<<<<<< HEAD
-=======
 	        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
->>>>>>> devD
 	        return "index";
 	    }
 }
--- src/main/java/com/GOBookingAPI/controller/MessageController.java
+++ src/main/java/com/GOBookingAPI/controller/MessageController.java
@@ -7,31 +7,13 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-<<<<<<< HEAD
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.GOBookingAPI.entities.Message;
-@Controller
-public class MessageController {
-
-	@MessageMapping("/message")
-	@SendTo("topic/public")
-	public String sendMessage(@Payload Message message) {
-		System.out.print("ádass");
-		return "âbcds";
-	}
-	
-	@MessageMapping("/addUser")
-    @SendTo("/topic/public")
-	public Message addPeople(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
-		headerAccessor.getSessionAttributes().put("senderId", message.getSenderId());
-		return message;
-	}
-=======
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -66,5 +48,4 @@ public class MessageController {
     public void sendToSpecificUser(@Payload Message message) {
         simpMessagingTemplate.convertAndSendToUser(message.getTo(), "/specific", message);
     }
->>>>>>> devD
 }
