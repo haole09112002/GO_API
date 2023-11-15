55ea02d25af47e0437705e8f5aa0e6481a41ac60
Tobi <dinhnguyen2002asd@gmail.com>
2023-11-15 13:41:24 +0700

    update theo lol dung

--- src/main/java/com/GOBookingAPI/payload/response/RegisterResponse.java
+++ src/main/java/com/GOBookingAPI/payload/response/RegisterResponse.java
@@ -2,11 +2,13 @@ package com.GOBookingAPI.payload.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class RegisterResponse {
 	private String message;
 }
