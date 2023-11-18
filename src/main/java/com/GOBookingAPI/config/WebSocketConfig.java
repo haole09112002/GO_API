0b6542e5be263a644553b3f50d278e72c8dbfe23
Tobi <dinhnguyen2002asd@gmail.com>
2023-11-19 00:03:18 +0700

    fix error

--- src/main/java/com/GOBookingAPI/config/WebSocketConfig.java
+++ src/main/java/com/GOBookingAPI/config/WebSocketConfig.java
@@ -36,7 +36,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
-		.setAllowedOrigins("http://127.0.0.1:5500" ,"http://localhost:5500" ,"http://localhost:3000" )
+		.setAllowedOrigins("http://127.0.0.1:5500" ,"http://localhost:5500" ,"http://localhost:3000")
 		.setHandshakeHandler(new UserHandshakeHandler(userService))
 		.withSockJS();
 //		registry.addEndpoint("/ws");
