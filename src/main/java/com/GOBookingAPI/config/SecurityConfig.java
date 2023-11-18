d7719b7def006080c74e23621ed6c5af76f1ab59
Le Van Hao <=>
2023-11-18 23:18:57 +0700

    .

--- src/main/java/com/GOBookingAPI/config/SecurityConfig.java
+++ src/main/java/com/GOBookingAPI/config/SecurityConfig.java
@@ -3,17 +3,15 @@ package com.GOBookingAPI.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import com.GOBookingAPI.security.Token.GoogleEntryPoint;
 import com.GOBookingAPI.security.Token.GoogleFilter;
 import com.GOBookingAPI.security.Token.GoogleProvider;
@@ -22,38 +20,39 @@ import com.GOBookingAPI.security.Token.GoogleProvider;
 @EnableMethodSecurity
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig implements WebMvcConfigurer{
-	
-	@Autowired
-	GoogleEntryPoint entryPoint;
-	
-	@Autowired
-	GoogleProvider provider;
-	
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http ) throws Exception{
-		
-		
-		http.cors().disable();
-        http.csrf().disable();
-		http.authorizeRequests().requestMatchers("/home/**").permitAll();
-		http.authorizeRequests().requestMatchers("/bookings/**").permitAll();
-		http.authorizeRequests().requestMatchers("/","/ws/**").authenticated().anyRequest().authenticated();
-		http.exceptionHandling().authenticationEntryPoint(entryPoint);
-		http.addFilterBefore(new GoogleFilter(), BasicAuthenticationFilter.class);
-		http.authenticationProvider(provider);
-		return http.build();
-	}
+public class SecurityConfig {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("*")
-				.allowedMethods("*")
-				.allowedHeaders("*")
-				.allowCredentials(true)
-				.maxAge(3600);
+    @Autowired
+    GoogleEntryPoint entryPoint;
+
+    @Autowired
+    GoogleProvider provider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
+        http.cors().and().csrf().disable();
+        http.authorizeRequests().requestMatchers("/home/**").permitAll();
+        http.authorizeRequests().requestMatchers("/bookings/**").permitAll();
+        http.authorizeRequests().requestMatchers("/", "/ws/**").authenticated().anyRequest().authenticated();
+        http.exceptionHandling().authenticationEntryPoint(entryPoint);
+        http.addFilterBefore(new GoogleFilter(), BasicAuthenticationFilter.class);
+        http.authenticationProvider(provider);
+        return http.build();
+    }
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedMethod("*");
+		config.addAllowedHeader("*");
+		config.setAllowCredentials(true);
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
-	
-	
 }
