07b6beeca50404cc2b752135db868b69c388ae2f
Tobi <dinhnguyen2002asd@gmail.com>
2023-12-04 20:38:07 +0700

    Test VNpay

--- src/main/java/com/GOBookingAPI/controller/PaymentController.java
+++ src/main/java/com/GOBookingAPI/controller/PaymentController.java
@@ -32,88 +32,94 @@ public class PaymentController {
 	@Autowired
 	private IPaymentService paymentService;
 	
-	@PostMapping
+	@GetMapping("/check")
 	public ResponseEntity<?> createPayment(@RequestParam Map<String, String> req) {
 		paymentService.handlePaymentTransaction(req);
         return ResponseEntity.ok("payment");
 	}
 	
+	@PostMapping("/returnUrl")
+	public ResponseEntity<?> test(@RequestParam Map<String, String> req) {
+        return ResponseEntity.ok("payment");
+	}
+	
 	@GetMapping("/create")
 	public ResponseEntity<?> create() throws UnsupportedEncodingException{
-//		  String vnp_Version = "2.1.0";
-//	        String vnp_Command = "pay";
-	        String orderType = "other";
-//	        long amount = Integer.parseInt(req.getParameter("amount"))*100;
-//	        String bankCode = req.getParameter("bankCode");
-//	        
-	        String vnp_TxnRef = VNPayConfig.getRandomNumber(8);
-//	        String vnp_IpAddr = VNPayConfig.getIpAddress(req);
+// //		  String vnp_Version = "2.1.0";
+// //	        String vnp_Command = "pay";
+// 	        String orderType = "other";
+// //	        long amount = Integer.parseInt(req.getParameter("amount"))*100;
+// //	        String bankCode = req.getParameter("bankCode");
+// //	        
+// 	        String vnp_TxnRef = VNPayConfig.getRandomNumber(8);
+// //	        String vnp_IpAddr = VNPayConfig.getIpAddress(req);
 
-	        String vnp_TmnCode = VNPayConfig.vnp_TmnCode;
+// 	        String vnp_TmnCode = VNPayConfig.vnp_TmnCode;
 	        
-	        long amount = 1000000L ;
+// 	        long amount = 1000000L ;
 	        
-	        Map<String, String> vnp_Params = new HashMap<>();
-	        vnp_Params.put("vnp_Version", VNPayConfig.vnp_Version);
-	        vnp_Params.put("vnp_Command", VNPayConfig.vnp_Command);
-	        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
-	        vnp_Params.put("vnp_Amount", String.valueOf(amount));
-	        vnp_Params.put("vnp_CurrCode", "VND");
-	        vnp_Params.put("vnp_BankCode", "NCB");
-	        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
-	        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
-	        vnp_Params.put("vnp_Locale", "vn");
-	        vnp_Params.put("vnp_ReturnUrl", VNPayConfig.vnp_ReturnUrl);
-	        vnp_Params.put("vnp_IpAddr", "localhost:8080");
-	        vnp_Params.put("vnp_OrderType", orderType);
-	        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
-	        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-	        String vnp_CreateDate = formatter.format(cld.getTime());
-	        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+// 	        Map<String, String> vnp_Params = new HashMap<>();
+// 	        vnp_Params.put("vnp_Version", VNPayConfig.vnp_Version);
+// 	        vnp_Params.put("vnp_Command", VNPayConfig.vnp_Command);
+// 	        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+// 	        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+// 	        vnp_Params.put("vnp_CurrCode", "VND");
+// 	        vnp_Params.put("vnp_BankCode", "NCB");
+// 	        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+// 	        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+// 	        vnp_Params.put("vnp_Locale", "vn");
+// 	        vnp_Params.put("vnp_ReturnUrl", VNPayConfig.vnp_ReturnUrl);
+// 	        vnp_Params.put("vnp_IpAddr", "localhost:8080");
+// 	        vnp_Params.put("vnp_OrderType", orderType);
+// 	        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+// 	        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+// 	        String vnp_CreateDate = formatter.format(cld.getTime());
+// 	        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
 	        	
-	        cld.add(Calendar.MINUTE, 15);
-	        String vnp_ExpireDate = formatter.format(cld.getTime());
-	        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+// 	        cld.add(Calendar.MINUTE, 15);
+// 	        String vnp_ExpireDate = formatter.format(cld.getTime());
+// 	        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 	        
-	        List fieldNames = new ArrayList(vnp_Params.keySet());
-	        Collections.sort(fieldNames);
-	        StringBuilder hashData = new StringBuilder();
-	        StringBuilder query = new StringBuilder();
-	        Iterator itr = fieldNames.iterator();
-	        while (itr.hasNext()) {
-	            String fieldName = (String) itr.next();
-	            String fieldValue = (String) vnp_Params.get(fieldName);
-	            if ((fieldValue != null) && (fieldValue.length() > 0)) {
-	                //Build hash data
-	                hashData.append(fieldName);
-	                hashData.append('=');
-	                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-	                //Build query
-	                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
-	                query.append('=');
-	                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-	                if (itr.hasNext()) {
-	                    query.append('&');
-	                    hashData.append('&');
-	                }
-	            }
-	        }
-	        String queryUrl = query.toString();
-	        String vnp_SecureHash = VNPayConfig.hmacSHA512(VNPayConfig.secretKey, hashData.toString());
-	        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
-	        String paymentUrl = VNPayConfig.vnp_PayUrl + "?" + queryUrl;
+// 	        List fieldNames = new ArrayList(vnp_Params.keySet());
+// 	        Collections.sort(fieldNames);
+// 	        StringBuilder hashData = new StringBuilder();
+// 	        StringBuilder query = new StringBuilder();
+// 	        Iterator itr = fieldNames.iterator();
+// 	        while (itr.hasNext()) {
+// 	            String fieldName = (String) itr.next();
+// 	            String fieldValue = (String) vnp_Params.get(fieldName);
+// 	            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+// 	                //Build hash data
+// 	                hashData.append(fieldName);
+// 	                hashData.append('=');
+// 	                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+// 	                //Build query
+// 	                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+// 	                query.append('=');
+// 	                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+// 	                if (itr.hasNext()) {
+// 	                    query.append('&');
+// 	                    hashData.append('&');
+// 	                }
+// 	            }
+// 	        }
+// 	        String queryUrl = query.toString();
+// 	        String vnp_SecureHash = VNPayConfig.hmacSHA512(VNPayConfig.secretKey, hashData.toString());
+// 	        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+// 	        String paymentUrl = VNPayConfig.vnp_PayUrl + "?" + queryUrl;
 	        
-	        PaymentResponse DTO = new PaymentResponse();
-	        DTO.setStatus("Ok");
-	        DTO.setMessage("Successfully");
-	        DTO.setURL(paymentUrl);
-//	        com.google.gson.JsonObject job = new JsonObject();
-//	        job.addProperty("code", "00");
-//	        job.addProperty("message", "success");
-//	        job.addProperty("data", paymentUrl);
-//	        Gson gson = new Gson();
-//	        resp.getWriter().write(gson.toJson(job));
-			return ResponseEntity.ok(DTO);
+// 	        PaymentResponse DTO = new PaymentResponse();
+// 	        DTO.setStatus("Ok");
+// 	        DTO.setMessage("Successfully");
+// 	        DTO.setURL(paymentUrl);
+// //	        com.google.gson.JsonObject job = new JsonObject();
+// //	        job.addProperty("code", "00");
+// //	        job.addProperty("message", "success");
+// //	        job.addProperty("data", paymentUrl);
+// //	        Gson gson = new Gson();
+// //	        resp.getWriter().write(gson.toJson(job));
+			System.out.prinln("test");
+			return ResponseEntity.ok("Test");
 	}
 //	@ModelAttribute PaymentRequest req
 	
