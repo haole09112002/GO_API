d38213bf5a56f582b2a770569f84c10a00f9626b
Tobi <dinhnguyen2002asd@gmail.com>
2023-12-05 23:17:44 +0700

    Doi + thêm 7 h

--- src/main/java/com/GOBookingAPI/controller/PaymentController.java
+++ src/main/java/com/GOBookingAPI/controller/PaymentController.java
@@ -73,6 +73,7 @@ public class PaymentController {
 
         Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        cld.add(Calendar.HOUR, 7);
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
         	
