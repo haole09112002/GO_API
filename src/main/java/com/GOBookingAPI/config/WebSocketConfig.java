3800d3410b105ca3fab860028eca61fad446c468
Le Van Hao <=>
2023-12-11 23:16:57 +0700

    change setAllowedOrigins

--- src/main/java/com/GOBookingAPI/config/WebSocketConfig.java
+++ src/main/java/com/GOBookingAPI/config/WebSocketConfig.java
@@ -29,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
 				.addInterceptors(customHandshakeInterceptor())
-		.setAllowedOrigins("http://127.0.0.1:5500" ,"http://localhost:5500" ,"http://localhost:3000","https://goapi-production-9e3a.up.railway.app", "*")
+		.setAllowedOrigins("http://127.0.0.1:5500" ,"http://localhost:5500" ,"http://localhost:3000","https://forlorn-bite-production.up.railway.app")
 		.setHandshakeHandler(new UserHandshakeHandler(userService))
 		.withSockJS();
 	}
