68556ced4c76d8cd8950a881cae55430030a70f5
Le Van Hao <=>
2023-12-12 00:43:44 +0700

    fixbug

--- src/main/java/com/GOBookingAPI/controller/WebSocketController.java
+++ src/main/java/com/GOBookingAPI/controller/WebSocketController.java
@@ -17,6 +17,7 @@ import com.sun.security.auth.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -24,69 +25,63 @@ import java.security.Principal;
 @RestController
 public class WebSocketController {
 
-	@Autowired
-	private IMessageService messageService;
-	
-	@Autowired
-	private IWebSocketService webSocketService;
+    @Autowired
+    private IMessageService messageService;
 
-	@Autowired
-	private IBookingService bookingService;
+    @Autowired
+    private IWebSocketService webSocketService;
 
-	@Autowired
-	private ManagerBooking managerBooking;
+    @Autowired
+    private IBookingService bookingService;
 
-	@Autowired
-	private ManagerLocation managerLocation;
+    @Autowired
+    private ManagerBooking managerBooking;
 
-	@Autowired
-	private IPaymentService paymentService;
+    @Autowired
+    private ManagerLocation managerLocation;
 
-	@Autowired
-	private IUserService userService;
-	
+    @Autowired
+    private IPaymentService paymentService;
+
+    @Autowired
+    private IUserService userService;
+
     @MessageMapping("/location")
-    public void sendLocation(final LocationWebSocketRequest location ) throws Exception {
-    	webSocketService.ListenLocationDriver(location);
+    public void sendLocation(final LocationWebSocketRequest location) throws Exception {
+        webSocketService.ListenLocationDriver(location);
     }
-    
+
     @MessageMapping("/message_send")
     public void sendToSpecificUser(@Payload CreateMessageRequest message) {
-    	System.out.println("====> receive message: " + message.toString());
-		Message mess =  messageService.createMessage(message);
-    	webSocketService.sendMessagePrivate(mess);
+        System.out.println("====> receive message: " + message.toString());
+        Message mess = messageService.createMessage(message);
+        webSocketService.sendMessagePrivate(mess);
     }
 
-	@MessageMapping("/booking_status")
-	public void processChangeBookingStatus(@Payload BookingStatusPacketRequest req) {
-		User user = userService.getById(req.getUid());
-		Booking booking = bookingService.changeBookingStatusAndNotify(user.getEmail(), req.getBookingId(), req.getBookingStatus());
-		if(booking != null){
-			webSocketService.notifyBookingStatusToCustomer(booking.getCustomer().getId(), new BookingStatusResponse(booking.getId(), booking.getStatus()));
-			if (booking.getDriver() != null){
-				webSocketService.notifyBookingStatusToCustomer(booking.getDriver().getId(), new BookingStatusResponse(booking.getId(), booking.getStatus()));   //
-				System.out.println("===> notify to driver: " + (new BookingStatusResponse(booking.getId(), booking.getStatus())).toString());
-				if(booking.getStatus().equals(BookingStatus.WAITING_REFUND)){
-					System.out.println("Booking status : " + BookingStatus.WAITING_REFUND);
-					managerBooking.deleteData(booking.getDriver().getId());
-					managerLocation.updateDriverStatus(booking.getDriver().getId(), DriverStatus.FREE);
-					paymentService.refundPayment(booking); // todo bug
+    @MessageMapping("/booking_status")
+    public void processChangeBookingStatus(@Payload BookingStatusPacketRequest req) {
+        User user = userService.getById(req.getUid());
+        Booking booking = bookingService.changeBookingStatusAndNotify(user.getEmail(), req.getBookingId(), req.getBookingStatus());
+        if (booking != null) {
+            webSocketService.notifyBookingStatusToCustomer(booking.getCustomer().getId(), new BookingStatusResponse(booking.getId(), booking.getStatus()));
+            if (booking.getDriver() != null) {
+                webSocketService.notifyBookingStatusToCustomer(booking.getDriver().getId(), new BookingStatusResponse(booking.getId(), booking.getStatus()));   //
+                System.out.println("===> notify to driver: " + (new BookingStatusResponse(booking.getId(), booking.getStatus())).toString());
+                if (booking.getStatus().equals(BookingStatus.WAITING_REFUND)) {
+                    System.out.println("Booking status : " + BookingStatus.WAITING_REFUND);
+                    managerBooking.deleteData(booking.getDriver().getId());
+                    managerLocation.updateDriverStatus(booking.getDriver().getId(), DriverStatus.FREE);
+                    paymentService.refundPayment(booking); // todo bug
 //					bookingService.changeBookingStatusForAdmin(booking.getId(), BookingStatus.REFUNDED); //todo debug
-				}
-			}
-		}
-	}
+                }
+            }
+        }
+    }
 
-	@MessageMapping("/current_booking")
-	public void getCurrentBooking(Principal principal) {
-    	if(principal != null)
-		{
-			System.out.println("==> principal.getName(): " + principal.getName());
-			User user = userService.findByEmail(principal.getName());
-			webSocketService.notifyBookingStatusToCustomer(user.getId(), new BookingStatusResponse(188574, BookingStatus.WAITING_REFUND));
-		}else {
-			System.out.println("==> principal.getName(): NULL");
-
-		}
-	}
+    @MessageMapping("/current_booking")
+    public void getCurrentBooking() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userService.getByEmail(email);
+        webSocketService.notifyBookingStatusToCustomer(user.getId(), new BookingStatusResponse(188574, BookingStatus.WAITING_REFUND));
+    }
 }
