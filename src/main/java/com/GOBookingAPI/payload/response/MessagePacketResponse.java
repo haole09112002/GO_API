739d23795843a4b020077e7933cecb92fbb7ec72
Le Van Hao <=>
2023-12-14 21:48:11 +0700

    fixbug minor

--- src/main/java/com/GOBookingAPI/payload/response/MessagePacketResponse.java
+++ src/main/java/com/GOBookingAPI/payload/response/MessagePacketResponse.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Setter
 public class MessagePacketResponse extends CreateMessageRequest {
 
-    private Date createAt;
+    private String createAt;
 
     @Override
     public String toString() {
