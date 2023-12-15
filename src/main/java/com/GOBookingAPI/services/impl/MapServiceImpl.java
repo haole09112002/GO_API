0688707b7142e210b8be8d336d1ba0aea5c6c3fd
Le Van Hao <=>
2023-12-15 15:51:07 +0700

    add log

--- src/main/java/com/GOBookingAPI/services/impl/MapServiceImpl.java
+++ src/main/java/com/GOBookingAPI/services/impl/MapServiceImpl.java
@@ -3,11 +3,13 @@ package com.GOBookingAPI.services.impl;
 import com.GOBookingAPI.payload.vietmap.VietMapResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -48,15 +50,41 @@ public class MapServiceImpl {
     }
 
     public String convertLocationToAddress(String location) {
-        String lng = location.split(",")[1];            // note :
-        String lat = location.split(",")[0];
-        String apiUrl = vietmapApiUrl + "/reverse/v3?apikey=" + vietmapApiKey
-                + "&lng=" + lng + "&lat=" + lat;
-        System.out.println("===> " + apiUrl);
-        String json = restTemplate.getForObject(apiUrl, String.class);
-        System.out.println("===> " + json);
-        JsonParser jsonParser = new JsonParser();
-        JsonObject jsonObject = jsonParser.parse(json).getAsJsonObject();
-        return jsonObject.get("address").getAsString();
+        try {
+            String[] coordinates = location.split(",");
+            String lng = coordinates[1].trim();
+            String lat = coordinates[0].trim();
+            String apiUrl = String.format("%s/reverse/v3?apikey=%s&lng=%s&lat=%s", vietmapApiUrl, vietmapApiKey, lng, lat);
+            System.out.println("===> " + apiUrl);
+
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
+
+            if (responseEntity.getStatusCode().is2xxSuccessful()) {
+                String json = responseEntity.getBody();
+
+                JsonParser jsonParser = new JsonParser();
+                JsonElement rootElement = jsonParser.parse(json);
+
+                if (rootElement.isJsonObject()) {
+                    JsonObject jsonObject = rootElement.getAsJsonObject();
+
+                    if (jsonObject.has("address")) {
+                        return jsonObject.get("address").getAsString();
+                    } else {
+                        System.out.println("Không có thông tin địa chỉ trong phản hồi JSON.");
+                        return null;
+                    }
+                } else {
+                    System.out.println("Phản hồi không phải là JSON Object.");
+                    return null;
+                }
+            } else {
+                System.out.println("Yêu cầu không thành công. Mã trạng thái: " + responseEntity.getStatusCodeValue());
+                return null;
+            }
+        } catch (Exception e) {
+            System.err.println("Lỗi chuyển đổi vị trí thành địa chỉ: " + e.getMessage());
+            return null;
+        }
     }
 }
