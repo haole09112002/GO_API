40f71c68db9e0673dc60f2f1e511cf26c0557d86
Le Van Hao <=>
2023-12-16 10:10:55 +0700

    fixbug customer endpoint

--- src/main/java/com/GOBookingAPI/controller/CustomerController.java
+++ src/main/java/com/GOBookingAPI/controller/CustomerController.java
@@ -22,7 +22,7 @@ public class CustomerController {
 	@Autowired
 	private IUserService userService;
 
-	@GetMapping("/{id}")
+	@GetMapping(value = {"/{id}", "/"})
 	@PreAuthorize("hasRole('CUSTOMER') or hasRole('ADMIN')")
 	public ResponseEntity<?> getCustomer(@PathVariable(required = false) int id){
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
