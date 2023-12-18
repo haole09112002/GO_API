a2962df4c341b5894de339e1986fe21e41f57e90
Le Van Hao <=>
2023-12-18 14:42:23 +0700

    fixbug

--- src/main/java/com/GOBookingAPI/config/ApiConfig.java
+++ src/main/java/com/GOBookingAPI/config/ApiConfig.java
@@ -1,34 +1,12 @@
 package com.GOBookingAPI.config;
 
-import com.cloudinary.Cloudinary;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
 public class ApiConfig {
-    @Value("${cloudinary.api.key}")
-    private String apiKey;
-
-    @Value("${cloudinary.api.name}")
-    private String name;
-
-    @Value("${cloudinary.api.secret}")
-    private String apiSecret;
-
-    @Bean
-    public Cloudinary cloudinary(){
-        Map<String, String> config = new HashMap<>();
-        config.put("cloud_name",this.name);
-        config.put("api_key", this.apiKey);
-        config.put("api_secret", this.apiSecret);
-        return new Cloudinary(config);
-    }
 
     @Bean
     public RestTemplate restTemplate() {
