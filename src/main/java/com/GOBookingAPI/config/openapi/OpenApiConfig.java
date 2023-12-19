7a072a2c7db0fdf81875cc484bc8b6be9728602a
Le Van Hao <=>
2023-12-19 22:58:52 +0700

    openapi doc

--- src/main/java/com/GOBookingAPI/config/openapi/OpenApiConfig.java
+++ src/main/java/com/GOBookingAPI/config/openapi/OpenApiConfig.java
@@ -1,24 +1,20 @@
-package com.GOBookingAPI.config.swagger;
+package com.GOBookingAPI.config.openapi;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.List;
 
-//@Configuration
-//@EnableSwagger2p
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//}
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of (
+                        new Server().url("https://forlorn-bite-production.up.railway.app"),
+                        new Server().url("http://localhost:8080")
+                ));
+    }
+}
