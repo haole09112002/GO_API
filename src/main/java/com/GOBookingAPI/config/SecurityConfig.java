aca175387b5a2b06a7901033eaf65d4ba68a6862
Le Van Hao <=>
2023-12-19 22:31:19 +0700

    fixbug

--- src/main/java/com/GOBookingAPI/config/SecurityConfig.java
+++ src/main/java/com/GOBookingAPI/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-        .allowedOrigins("http://127.0.0.1:5500" , "http://127.0.0.1:3000" , "http://localhost:3000", "https://go-webapp.vercel.app")
+        .allowedOrigins("http://127.0.0.1:5500" , "http://127.0.0.1:3000" , "http://localhost:3000", "https://go-webapp.vercel.app", "https://forlorn-bite-production.up.railway.app/", "http://forlorn-bite-production.up.railway.app/")
         .allowedHeaders("*")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH")
         .maxAge(-1)   // add maxAge
